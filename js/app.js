ee943005c6abc6013e2850da2e944e3d77b2c02a
Shir Bar Lev <shirblc@gmail.com>
2019-10-07 15:03:08 +0300

    fixed clicker for both cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-let numClicks = 0;
+let numClicksCat1 = 0;
+let numClicksCat2 = 0;
 
 document.getElementById("catPhoto1").addEventListener("click", increaseClickNum);
+document.getElementById("catPhoto2").addEventListener("click", increaseClickNum);
 
-function increaseClickNum()
+function increaseClickNum(e)
 {
-	numClicks++;
-	document.getElementById("counterNumber1").textContent = numClicks;
+	switch(e.target.id.substr(e.target.id.length - 1, 1))
+		{
+			case "1":
+				numClicksCat1++;
+				document.getElementById(`counterNumber1`).textContent = numClicksCat1;
+				break;
+			case "2":
+				numClicksCat2++;
+				document.getElementById(`counterNumber2`).textContent = numClicksCat2;
+				break;
+		}
 }
