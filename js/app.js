9b66c0af75ffdede65c2580f96c0abd74e4b0710
Shir Bar Lev <shirblc@gmail.com>
2019-10-07 14:34:58 +0300

    added second kitten photo + text

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 let numClicks = 0;
 
-document.getElementById("catPhoto").addEventListener("click", increaseClickNum);
+document.getElementById("catPhoto1").addEventListener("click", increaseClickNum);
 
 function increaseClickNum()
 {
 	numClicks++;
-	document.getElementById("counterNumber").textContent = numClicks;
+	document.getElementById("counterNumber1").textContent = numClicks;
 }
