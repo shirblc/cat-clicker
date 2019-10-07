6a2db465a46f942568dd9454520c56959cf54327
Shir Bar Lev <shirblc@gmail.com>
2019-10-07 10:26:51 +0300

    added user interaction function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+let numClicks = 0;
+
+document.getElementById("catPhoto").addEventListener("click", increaseClickNum);
+
+function increaseClickNum()
+{
+	numClicks++;
+	document.getElementById("counterNumber").textContent = numClicks;
+}
