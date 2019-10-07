d0b2c172ac5f4f4f125b7fd655bbb22fac7dc3c8
Shir Bar Lev <shirblc@gmail.com>
2019-10-07 17:06:22 +0300

    fixed the click function to accomodate new conditions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-let numClicksCat1 = 0;
-let numClicksCat2 = 0;
 let catsNames = ["Katy", "Ginger"];
 let catsURLS = ["1562-cute-little-cat.jpg","tiny-kitten-in-field.jpg"];
 let cats = [];
@@ -11,11 +9,11 @@ class Cat
 			this.catName = catName;
 			this.catNumber = catNumber;
 			this.catPicture = catPicture;
+			this.numClicks = 0;
 		}
 	}
 
-document.getElementById("catPhoto1").addEventListener("click", increaseClickNum);
-document.getElementById("catPhoto2").addEventListener("click", increaseClickNum);
+document.getElementById("catPhoto").addEventListener("click", increaseClickNum);
 
 buildNavigationMenu();
 initCats();
@@ -48,22 +46,15 @@ function showCatDetails(e)
 {
 	let chosenCatNum = e.target.id.substring(8,1);
 	
-	document.querySelector(".counterNumber").textContent = 0;
+	document.querySelector(".counterNumber").textContent = cats[chosenCatNum-1].numClicks;
 	document.querySelector(".kittenName").textContent = cats[chosenCatNum-1].catName;
 	document.querySelector(".catPhoto").setAttribute("src", cats[chosenCatNum-1].catPicture);
 }
 
 function increaseClickNum(e)
 {
-	switch(e.target.id.substr(e.target.id.length - 1, 1))
-		{
-			case "1":
-				numClicksCat1++;
-				document.getElementById(`counterNumber1`).textContent = numClicksCat1;
-				break;
-			case "2":
-				numClicksCat2++;
-				document.getElementById(`counterNumber2`).textContent = numClicksCat2;
-				break;
-		}
+	let catNum = e.target.id.substr(e.target.id.length - 1, 1);
+	
+	cats[catNum-1].numClicks++;
+	document.querySelector(".counterNumber").textContent = cats[chosenCatNum-1].numClicks;
 }
